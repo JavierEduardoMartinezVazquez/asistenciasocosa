1bde36fab47d854fe29f194e019d0ac7ff97121a
Javier Eduardo <al222010046@gmail.com>
2022-05-05 17:30:31 -0500

    avance

--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -7,6 +7,6 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-primary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#" center>Empleados</a>
+      <a class="navbar-brand" href="#" >Empleados</a>
   </nav>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\LoginController;
 
 
 
 //RUTA PRINCIPAL
-Route::get('/', HomeController::class);
-
-
+Route::get('/', LoginController::class);
+Route::get('/home', HomeController::class);
 Route::controller(EmpleadoController::class)->group(function(){
     Route::get('empleados', 'index');
     Route::get('empleados/create', 'create');
