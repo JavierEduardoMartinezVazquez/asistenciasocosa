cf4711f8be6770a1ee1efa1f9f4b446bd88b3882
Javier Eduardo <al222010046@gmail.com>
2022-05-09 16:57:05 -0500

    creacion de prueba

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\LoginController;
-
+use App\Http\Controllers\PruebaController;
 
 
 //RUTA PRINCIPAL
 Route::get('/', LoginController::class);
 Route::get('/home', HomeController::class);
+Route::get('/prueba', PruebaController::class);
 
 Route::controller(EmpleadoController::class)->group(function(){
     Route::get('empleados', 'index');
