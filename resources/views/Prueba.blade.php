f0d97dcec84e54fc8ab29ef2976889a46b1cc5ef
Javier Eduardo <al222010046@gmail.com>
2022-05-10 12:18:47 -0500

    Primera ventana de welcome solo interfaz

---
+++ resources/views/Prueba.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.plantilla')
+
+@section('tittle','Login')
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+@section('content')
+
+<div class="col-md-12 border border-danger">
+    </div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <b class="navbar-brand" href="#"> 
+        <p class="text-muted">SOCASA</p>
+        </b>
+    
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+<div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;"></ul>
+        <form class="col-1">
+            <button type="button" class="btn btn-outline-secondary">LOGIN</button>  
+        </form>
+        <form class="col-1">
+            <button type="button" class="btn btn-outline-secondary">LOGIN</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+<header>
+    <!-- Background image -->
+    <div class="p-5 text-center bg-image" style="height: 523px;">
+      <div class="mask">
+        <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-center">
+                    <div class="card-body">
+                        <h1 class="card-title">
+                            <div class="text-secondary">
+                              SISTEMA DE ASISTENCIA</h1>
+                        </div>
+                        <h1 class="display-6">Soluciones integrales para tu camion SOCASA S.A. C.V</h1>
+                        </div>
+                      </div>
+                </div>
+    </div>
+  </header>
+
+  <footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(255, 0, 0, 0.884);">
+        <div class="text-white">
+      © 2022 Copyright JET
+    </div>
+    <!-- Copyright -->
+  </footer>
+@endsection
+
