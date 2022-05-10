43d489f835f866d5d17702e88676bde36c9ab4cd
Javier Eduardo <al222010046@gmail.com>
2022-05-10 13:48:15 -0500

    Primera ventana de login solo interfaz

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -4,79 +4,58 @@
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 @section('content')
 
-<nav class="navbar navbar-dark bg-danger center">
-      <a class="navbar-brand" href="#">
-          <div class="container">
-              SISTEMA DE ASISTENCIA
-            </a>
-      </div>
+<div class="col-md-12 border border-danger">
     </div>
-</nav>
-<p>
 
-</p>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <p></p>
-        Inicio de Sesion
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <div class="navbar-brand" href="#"> 
+        <b class="text-muted">SOCASA</b>
       </div>
-    </div>
-</div>
-<p></p>
-  
     
-</nav>
-<section class="vh-100">
-    <div class="container-fluid h-custom">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-md-9 col-lg-6 col-xl-5">
-          <img src="https://empresas.divulgaciondinamica.es/wp-content/uploads/2020/06/create-idea-success_41910-189.jpg"
-            class="img-fluid" alt="image">
-        </div>
-        <div class="col-md-10 col-lg-6 col-xl-4 offset-xl-1">
-          <form>
-           <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-            </div>
-            
-  
-            <div class="divider d-flex align-items-center my-4">
-            </div>
-            
-            <div class="form-outline mb-3">
-                Login
-              </div>
-  
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-              <input type="email" id="form3Example3" class="form-control form-control-lg"
-                placeholder="ej:nombre.apellido@socasa.com.mx"/>
-              <label class="form-label" for="form3Example3">Correo Electronico</label>
-            </div> 
-  
-            <!-- Password input -->
-            <div class="form-outline mb-3">
-              <input type="password" id="form3Example4" class="form-control form-control-lg"
-              <label class="form-label" for="form3Example4">Contraseña</label>
-            </div>
-  
-            <div class="d-flex justify-content-between align-items-center">
-              
-  
-            <div class="text-center text-lg-start mt-4 pt-2">
-              <button type="button" class="btn btn-danger btn-lg
-                style="padding-left: 2.5rem; padding-right: 2.5rem;" >Iniciar sesion</button>      
-            </div>
-          </form>
-        </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+<div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg- navbar-nav-scroll"></ul>
+        <form class="col-2">
+          <a href="http://localhost/asistenciasocasa/public/home" class="btn btn-outline-secondary">REGISTRATE</a>
+        </form>
       </div>
-      <p></p>
-      <footer class="bg-light text-center text-lg-start">
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgb(202, 199, 199);">
-          © 2022 Copyright
-          <class="text-dark" href="https://">
-        </div>
-      </footer>
-        
+    </div>
+  </nav>
+  <br>
+<br>
+<br>
+<header>
+    <!-- Background image -->
+    <div class="p-5 text-center bg-image" style="height: 467px;">
+      <div class="mask">
+        <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-center">
+                    <div class="card-body">
+                        <h1 class="card-title">
+                            <div class="text-secondary">
+                              SISTEMA DE ASISTENCIA</h1>
+                        </div>
+                        <h1 class="display-6">Soluciones integrales para tu camion SOCASA S.A. C.V</h1>
+                        <form class="col-row-12">
+                            <br>
+                            <br>
+                            <a href="http://localhost/asistenciasocasa/public/home" class="btn btn-outline-secondary">LOGIN</a>
+                        </div>
+                      </div>
+                </div>
+    </div>
+  </header>
+
+  <footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(255, 0, 0, 0.884);">
+        <div class="text-white">
+      © 2022 Copyright JET
+    </div>
+    <!-- Copyright -->
+  </footer>
 @endsection
+
--- resources/views/Prueba.blade.php
+++ resources/views/Prueba.blade.php
@@ -4,51 +4,47 @@
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 @section('content')
 
-<div class="col-md-12 border border-danger">
-    </div>
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <b class="navbar-brand" href="#"> 
-        <p class="text-muted">SOCASA</p>
-        </b>
-    
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-<div class="collapse navbar-collapse" id="navbarScroll">
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;"></ul>
-        <form class="col-1">
-            <button type="button" class="btn btn-outline-secondary">LOGIN</button>  
-        </form>
-        <form class="col-1">
-            <button type="button" class="btn btn-outline-secondary">LOGIN</button>
-        </form>
-      </div>
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+
+        <li class="nav-item dropdown">
+            <a class="nav-link active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Inicio
+            </a>
+          </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Link
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </form>
     </div>
-  </nav>
+  </div>
+</nav>
 
 <header>
-    <!-- Background image -->
-    <div class="p-5 text-center bg-image" style="height: 523px;">
-      <div class="mask">
-        <div class="d-flex justify-content-center align-items-center h-100">
-                <div class="text-center">
-                    <div class="card-body">
-                        <h1 class="card-title">
-                            <div class="text-secondary">
-                              SISTEMA DE ASISTENCIA</h1>
-                        </div>
-                        <h1 class="display-6">Soluciones integrales para tu camion SOCASA S.A. C.V</h1>
-                        </div>
-                      </div>
-                </div>
-    </div>
+    <br>
+    <br>
+
+    aqui se mostrara la informacion de los empleados
+    
+    <br>
+    <br>
+    <br>
   </header>
 
   <footer class="bg-light text-center text-lg-start">
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(255, 0, 0, 0.884);">
+    <div class="text-center p-3" style="background-color: rgb(117, 117, 117);">
         <div class="text-white">
       © 2022 Copyright JET
     </div>
